5e321b7d819a00f6064cda5afac6c18e31577921
Shubham <raiandbala@outlook.com>
2022-06-07 16:26:32 +0530

    hotfix commit1

--- src/java1.java
+++ src/java1.java
@@ -2,5 +2,8 @@ public class java1{
 public static void main(String[] args) {
 	System.out.println("Jay Shree Ram");
 	System.out.println("Jay Mata Pitaji");
+	int a ,b;
+	a=2;b=3;
+	System.out.println("a");
 }
 }
